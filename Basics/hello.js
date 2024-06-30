af43824d8413ab3fa2bc7a3cdeb315086044c658
PRO0FESSOR <siddharth96549@gmail.com>
2024-06-30 14:29:57 +0530

    the basics

---
+++ Basics/hello.js
@@ -0,0 +1,23 @@
+console.log("hello");
+//static type checking
+// const message = "hello!";
+// message();
+//dosent return undefined like js 
+// const user = {
+//     name: "Daniel",
+//     age: 26,
+//   };
+//   user.location;
+//   uncalled functions,
+//   function flipCoin() {
+//     // Meant to be Math.random()
+//     return Math.random < 0.5
+//     ;
+//   }
+//automatically assign datatype
+// let msag = "hello";
+//explicit type
+// function greet(person:string,age:number){
+//     console.log(`hello ${person} your age is ${age}`);
+// }
+// greet("siddharth",6);
